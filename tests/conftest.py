6706605b013313d9380b4dab7903ab3eeabda332
Giovanni Pizzi <giovanni.pizzi@epfl.ch>
2021-06-08 09:18:12 +0200

    Adding one more test
    It is a KTlCl4 that didn't work before the changes
    Note: it requires a very specific threshold (1.05 or 1.06) to be identified
    as 2D.
    
    In addition, increasing the window size for Chrome tests, so screenshots will
    contain more useful information.

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,4 +7,7 @@ def chrome_options(chrome_options):
     """By default use these options with Chrome."""
     # https://pytest-selenium.readthedocs.io/en/latest/user_guide.html#id2
     chrome_options.add_argument("--headless")
+    # So screeshots contain more information - note that it's longer than larger
+    # as this will allow to see more content
+    chrome_options.add_argument("--window-size=1080,1920")
     return chrome_options
